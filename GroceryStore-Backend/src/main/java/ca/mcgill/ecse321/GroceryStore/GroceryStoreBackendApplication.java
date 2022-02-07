dfa144cd8837d0955f072db7bbc624d1bfd5bd0c
seb8stien <sebcantin70@gmail.com>
2022-02-07 16:37:46 -0500

    Restart: Initial commit of the backend application

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/GroceryStoreBackendApplication.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/GroceryStoreBackendApplication.java
@@ -2,10 +2,7 @@ package ca.mcgill.ecse321.GroceryStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
 @SpringBootApplication
 public class GroceryStoreBackendApplication {
 
@@ -13,8 +10,4 @@ public class GroceryStoreBackendApplication {
 		SpringApplication.run(GroceryStoreBackendApplication.class, args);
 	}
 
-	@RequestMapping("/")
- 	public String greeting(){
-    	return "Hello world!";
-	}
 }
