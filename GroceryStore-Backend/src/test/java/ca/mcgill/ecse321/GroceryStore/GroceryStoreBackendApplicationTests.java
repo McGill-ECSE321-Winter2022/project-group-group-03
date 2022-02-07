b7070b6e404afbe9ffebf858afd364f98d4ddfc7
seb8stien <sebcantin70@gmail.com>
2022-02-07 13:12:45 -0500

    Initial commit of the backend application

---
+++ GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/GroceryStore/GroceryStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.GroceryStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
