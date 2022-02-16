607a00c2c8232a587f6a59e69d7d5d854889a8bc
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2022-02-16 16:05:32 -0500

    added customer

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/model/Customer.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/model/Customer.java
@@ -1,164 +1,164 @@
-///*PLEASE DO NOT EDIT THIS CODE*/
-///*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
-//
-//package ca.mcgill.ecse321.GroceryStore.model;
-//import javax.persistence.*;
-//import java.util.*;
-//
-//// line 41 "../../../../../../model.ump"
-//// line 125 "../../../../../../model.ump"
-//// line 158 "../../../../../../model.ump"
-//// line 173 "../../../../../../model.ump"
-//// line 207 "../../../../../../model.ump"
-//@Entity
-//public class Customer
-//{
-//
-//  //------------------------
-//  // STATIC VARIABLES
-//  //------------------------
-//
-//  private static Map<String, Customer> customersByUsername = new HashMap<String, Customer>();
-//  private static Map<String, Customer> customersByEmail = new HashMap<String, Customer>();
-//
-//  //------------------------
-//  // MEMBER VARIABLES
-//  //------------------------
-//
-//  //Customer Attributes
-//  @Id
-//  private String username;
-//  private String password;
-//  private String email;
-//  private String address;
-//
-//  //Customer Associations
-//
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
+
+package ca.mcgill.ecse321.GroceryStore.model;
+import javax.persistence.*;
+import java.util.*;
+
+// line 41 "../../../../../../model.ump"
+// line 125 "../../../../../../model.ump"
+// line 158 "../../../../../../model.ump"
+// line 173 "../../../../../../model.ump"
+// line 207 "../../../../../../model.ump"
+@Entity
+public class Customer
+{
+
+  //------------------------
+  // STATIC VARIABLES
+  //------------------------
+
+  private static Map<String, Customer> customersByUsername = new HashMap<String, Customer>();
+  private static Map<String, Customer> customersByEmail = new HashMap<String, Customer>();
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Customer Attributes
+  @Id
+  private String username;
+  private String password;
+  private String email;
+  private String address;
+
+  //Customer Associations
+
 //  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
 //  @JoinColumn(name = "customer_username")
 //  private List<Order> order = new ArrayList<>();
-//
-//  //------------------------
-//  // CONSTRUCTOR
-//  //------------------------
-//
-//  public Customer(String aUsername, String aPassword, String aEmail, String aAddress)
-//  {
-//    password = aPassword;
-//    address = aAddress;
-//    if (!setUsername(aUsername))
-//    {
-//      throw new RuntimeException("Cannot create due to duplicate username. See http://manual.umple.org?RE003ViolationofUniqueness.html");
-//    }
-//    if (!setEmail(aEmail))
-//    {
-//      throw new RuntimeException("Cannot create due to duplicate email. See http://manual.umple.org?RE003ViolationofUniqueness.html");
-//    }
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Customer(String aUsername, String aPassword, String aEmail, String aAddress)
+  {
+    password = aPassword;
+    address = aAddress;
+    if (!setUsername(aUsername))
+    {
+      throw new RuntimeException("Cannot create due to duplicate username. See http://manual.umple.org?RE003ViolationofUniqueness.html");
+    }
+    if (!setEmail(aEmail))
+    {
+      throw new RuntimeException("Cannot create due to duplicate email. See http://manual.umple.org?RE003ViolationofUniqueness.html");
+    }
 //    order = new ArrayList<Order>();
-//  }
-//
-//  public Customer() {
-//
-//  }
-//
-//  //------------------------
-//  // INTERFACE
-//  //------------------------
-//
-//  public boolean setUsername(String aUsername)
-//  {
-//    boolean wasSet = false;
-//    String anOldUsername = getUsername();
-//    if (anOldUsername != null && anOldUsername.equals(aUsername)) {
-//      return true;
-//    }
-//    if (hasWithUsername(aUsername)) {
-//      return wasSet;
-//    }
-//    username = aUsername;
-//    wasSet = true;
-//    if (anOldUsername != null) {
-//      customersByUsername.remove(anOldUsername);
-//    }
-//    customersByUsername.put(aUsername, this);
-//    return wasSet;
-//  }
-//
-//  public boolean setPassword(String aPassword)
-//  {
-//    boolean wasSet = false;
-//    password = aPassword;
-//    wasSet = true;
-//    return wasSet;
-//  }
-//
-//  public boolean setEmail(String aEmail)
-//  {
-//    boolean wasSet = false;
-//    String anOldEmail = getEmail();
-//    if (anOldEmail != null && anOldEmail.equals(aEmail)) {
-//      return true;
-//    }
-//    if (hasWithEmail(aEmail)) {
-//      return wasSet;
-//    }
-//    email = aEmail;
-//    wasSet = true;
-//    if (anOldEmail != null) {
-//      customersByEmail.remove(anOldEmail);
-//    }
-//    customersByEmail.put(aEmail, this);
-//    return wasSet;
-//  }
-//
-//  public boolean setAddress(String aAddress)
-//  {
-//    boolean wasSet = false;
-//    address = aAddress;
-//    wasSet = true;
-//    return wasSet;
-//  }
-//
-//  public String getUsername()
-//  {
-//    return username;
-//  }
-//  /* Code from template attribute_GetUnique */
-//  public static Customer getWithUsername(String aUsername)
-//  {
-//    return customersByUsername.get(aUsername);
-//  }
-//  /* Code from template attribute_HasUnique */
-//  public static boolean hasWithUsername(String aUsername)
-//  {
-//    return getWithUsername(aUsername) != null;
-//  }
-//
-//  public String getPassword()
-//  {
-//    return password;
-//  }
-//
-//  public String getEmail()
-//  {
-//    return email;
-//  }
-//  /* Code from template attribute_GetUnique */
-//  public static Customer getWithEmail(String aEmail)
-//  {
-//    return customersByEmail.get(aEmail);
-//  }
-//  /* Code from template attribute_HasUnique */
-//  public static boolean hasWithEmail(String aEmail)
-//  {
-//    return getWithEmail(aEmail) != null;
-//  }
-//
-//  public String getAddress()
-//  {
-//    return address;
-//  }
-//  /* Code from template association_GetMany */
+  }
+
+  public Customer() {
+
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setUsername(String aUsername)
+  {
+    boolean wasSet = false;
+    String anOldUsername = getUsername();
+    if (anOldUsername != null && anOldUsername.equals(aUsername)) {
+      return true;
+    }
+    if (hasWithUsername(aUsername)) {
+      return wasSet;
+    }
+    username = aUsername;
+    wasSet = true;
+    if (anOldUsername != null) {
+      customersByUsername.remove(anOldUsername);
+    }
+    customersByUsername.put(aUsername, this);
+    return wasSet;
+  }
+
+  public boolean setPassword(String aPassword)
+  {
+    boolean wasSet = false;
+    password = aPassword;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setEmail(String aEmail)
+  {
+    boolean wasSet = false;
+    String anOldEmail = getEmail();
+    if (anOldEmail != null && anOldEmail.equals(aEmail)) {
+      return true;
+    }
+    if (hasWithEmail(aEmail)) {
+      return wasSet;
+    }
+    email = aEmail;
+    wasSet = true;
+    if (anOldEmail != null) {
+      customersByEmail.remove(anOldEmail);
+    }
+    customersByEmail.put(aEmail, this);
+    return wasSet;
+  }
+
+  public boolean setAddress(String aAddress)
+  {
+    boolean wasSet = false;
+    address = aAddress;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getUsername()
+  {
+    return username;
+  }
+  /* Code from template attribute_GetUnique */
+  public static Customer getWithUsername(String aUsername)
+  {
+    return customersByUsername.get(aUsername);
+  }
+  /* Code from template attribute_HasUnique */
+  public static boolean hasWithUsername(String aUsername)
+  {
+    return getWithUsername(aUsername) != null;
+  }
+
+  public String getPassword()
+  {
+    return password;
+  }
+
+  public String getEmail()
+  {
+    return email;
+  }
+  /* Code from template attribute_GetUnique */
+  public static Customer getWithEmail(String aEmail)
+  {
+    return customersByEmail.get(aEmail);
+  }
+  /* Code from template attribute_HasUnique */
+  public static boolean hasWithEmail(String aEmail)
+  {
+    return getWithEmail(aEmail) != null;
+  }
+
+  public String getAddress()
+  {
+    return address;
+  }
+  /* Code from template association_GetMany */
 //  public Order getOrder(int index)
 //  {
 //    Order aOrder = order.get(index);
@@ -193,7 +193,7 @@
 //  {
 //    return 0;
 //  }
-//  /* Code from template association_AddUnidirectionalMany */
+  /* Code from template association_AddUnidirectionalMany */
 //  public boolean addOrder(Order aOrder)
 //  {
 //    boolean wasAdded = false;
@@ -202,7 +202,7 @@
 //    wasAdded = true;
 //    return wasAdded;
 //  }
-//
+
 //  public boolean removeOrder(Order aOrder)
 //  {
 //    boolean wasRemoved = false;
@@ -213,7 +213,7 @@
 //    }
 //    return wasRemoved;
 //  }
-//  /* Code from template association_AddIndexControlFunctions */
+  /* Code from template association_AddIndexControlFunctions */
 //  public boolean addOrderAt(Order aOrder, int index)
 //  {
 //    boolean wasAdded = false;
@@ -227,7 +227,7 @@
 //    }
 //    return wasAdded;
 //  }
-//
+
 //  public boolean addOrMoveOrderAt(Order aOrder, int index)
 //  {
 //    boolean wasAdded = false;
@@ -245,21 +245,21 @@
 //    }
 //    return wasAdded;
 //  }
-//
+
 //  public void delete()
 //  {
 //    customersByUsername.remove(getUsername());
 //    customersByEmail.remove(getEmail());
 //    order.clear();
 //  }
-//
-//
-//  public String toString()
-//  {
-//    return super.toString() + "["+
-//            "username" + ":" + getUsername()+ "," +
-//            "password" + ":" + getPassword()+ "," +
-//            "email" + ":" + getEmail()+ "," +
-//            "address" + ":" + getAddress()+ "]";
-//  }
-//}
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "username" + ":" + getUsername()+ "," +
+            "password" + ":" + getPassword()+ "," +
+            "email" + ":" + getEmail()+ "," +
+            "address" + ":" + getAddress()+ "]";
+  }
+}
