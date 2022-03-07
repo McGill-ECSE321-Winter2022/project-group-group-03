617e774a108e9b440fa4e4609a10f27fa4ad367d
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2022-03-07 12:22:16 -0500

    PickupOrder and Holiday service classes

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/service/HolidayService.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/service/HolidayService.java
@@ -1,7 +1,49 @@
 package ca.mcgill.ecse321.GroceryStore.service;
 
+import ca.mcgill.ecse321.GroceryStore.dao.HolidayRepository;
+import ca.mcgill.ecse321.GroceryStore.model.Holiday;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HolidayService {
+    @Autowired
+    HolidayRepository holidayRepository;
+
+    @Transactional
+    public Holiday createHoliday (String name,Date startDate, Date endDate){
+        Holiday holiday = new Holiday();
+        holiday.setName(name);
+        holiday.setStartDate(startDate);
+        holiday.setEndDate(endDate);
+        holidayRepository.save(holiday);
+
+        return holiday;
+    }
+    public List<Holiday> getAllHolidays(){
+        List<Holiday> holidays = new ArrayList<>();
+        for (Holiday holiday:holidayRepository.findAll() ) {
+            holidays.add(holiday);
+        }
+
+        return holidays;
+    }
+
+    @Transactional
+    public Holiday getHoliday(String name){
+        Holiday holiday = holidayRepository.findByName(name);
+        return holiday;
+    }
+    @Transactional
+    public void deleteHoliday(String name){
+        holidayRepository.deleteById(name);
+    }
+
+
 
 
 }
