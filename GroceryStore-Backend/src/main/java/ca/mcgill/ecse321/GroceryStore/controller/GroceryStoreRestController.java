ddfda5e3ee89d1530c25931ed6302bbab7e9cdfa
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2022-03-08 13:23:35 -0500

    created package and class for rest controller

---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/controller/GroceryStoreRestController.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.GroceryStore.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class GroceryStoreRestController {
+   // @Autowired
+    //private EventRegistrationService service;
+
+
+}
