6e80305208064dff298267be6fc9179812c176fc
Neel <neel.faucher@mail.mcgill.ca>
2022-03-08 14:27:37 -0500

    PurchasedItemDTO created

---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/dto/PurchasedItemDTO.java
@@ -0,0 +1,39 @@
+package ca.mcgill.ecse321.GroceryStore.dto;
+
+import ca.mcgill.ecse321.GroceryStore.model.Item;
+
+public class PurchasedItemDTO {
+
+    private Item item;
+    private int aItemQuantity;
+    private int aPurchasedItem;
+
+
+    public PurchasedItemDTO() {
+    }
+
+    //TODO: SuppressWarning
+    /*@SuppressWarnings("unchecked")
+    public PersonDto(String name) {
+        this(name, Collections.EMPTY_LIST);
+    }*/
+
+    public PurchasedItemDTO(Item item, int aItemQuantity, int aPurchasedItem) {
+        this.item = item;
+        this.aItemQuantity = aItemQuantity;
+        this.aPurchasedItem = aPurchasedItem;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getaPurchasedItem() {
+        return aPurchasedItem;
+    }
+
+    public int getaItemQuantity() {
+        return aItemQuantity;
+    }
+
+}
