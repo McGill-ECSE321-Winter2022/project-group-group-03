304872354883d1a0c32fd8eb4a68b742627e3ce3
seb8stien1 <sebastien.cantin@mail.mcgill.ca>
2022-03-11 16:00:38 -0500

    Filled Owner and Employee DTO

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/dto/EmployeeDTO.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/dto/EmployeeDTO.java
@@ -1,4 +1,58 @@
 package ca.mcgill.ecse321.GroceryStore.dto;
 
+
+import ca.mcgill.ecse321.GroceryStore.model.DeliveryOrder;
+import ca.mcgill.ecse321.GroceryStore.model.Employee;
+
 public class EmployeeDTO {
+    private String username;
+    private String password;
+    private String email;
+    private String address;
+
+    private List<WorkShiftDTO> workShifts;
+    private List<DeliveryOrderDTO> deliveryOrders;
+
+    public EmployeeDTO(){
+    }
+
+    @SuppressWarnings("unchecked")
+    public EmployeeDTO(String username, String password, String email, String address){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public List<WorkShiftDTO> getWorkShifts() {
+        return workShifts;
+    }
+
+    public List<DeliveryOrderDTO> getDeliveryOrders() {
+        return deliveryOrders;
+    }
+
+    public void setWorkShifts(List<WorkShiftDTO> workShifts) {
+        this.workShifts = workShifts;
+    }
+
+    public void setDeliveryOrders(List<DeliveryOrderDTO> deliveryOrders) {
+        this.deliveryOrders = deliveryOrders;
+    }
 }
