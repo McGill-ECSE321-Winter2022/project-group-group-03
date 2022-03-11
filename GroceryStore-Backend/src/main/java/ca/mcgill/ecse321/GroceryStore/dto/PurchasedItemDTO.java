586ed709909ec11e0bc251cf2c26720250199852
Neel <neel.faucher@mail.mcgill.ca>
2022-03-11 14:06:40 -0500

    PurchasedItemDTO created

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/dto/PurchasedItemDTO.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/dto/PurchasedItemDTO.java
@@ -35,5 +35,5 @@ public class PurchasedItemDTO {
     public int getaItemQuantity() {
         return aItemQuantity;
     }
-
+    
 }
---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/dto/WorkshiftDTO.java
@@ -0,0 +1,34 @@
+package ca.mcgill.ecse321.GroceryStore.dto;
+
+import ca.mcgill.ecse321.GroceryStore.model.WorkShift;
+
+import java.sql.Time;
+import java.time.DayOfWeek;
+
+public class WorkshiftDTO {
+
+    private Time startTime;
+    private Time endTime;
+    private int shiftID;
+
+    public WorkshiftDTO() {
+    }
+
+    public WorkshiftDTO(Time startTime, Time endTime, int shiftID) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.shiftID = shiftID;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+    public Time getEndTime() {
+        return endTime;
+    }
+    public int getShiftID() {
+        return shiftID;
+    }
+
+
+}
