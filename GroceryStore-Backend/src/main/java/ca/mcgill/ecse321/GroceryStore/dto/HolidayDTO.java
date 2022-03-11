e7d7721e79c7e2acefc9730ae70c18448afee014
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2022-03-11 13:54:02 -0500

    added HolidayDTO

---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/dto/HolidayDTO.java
@@ -0,0 +1,36 @@
+package ca.mcgill.ecse321.GroceryStore.dto;
+
+import ca.mcgill.ecse321.GroceryStore.model.Holiday;
+
+import java.sql.Date;
+
+public class HolidayDTO {
+    private String name;
+    private Date startDate;
+    private Date endDate;
+
+    public HolidayDTO(){
+
+    }
+
+    public HolidayDTO(String aName, Date aStartDate, Date aEndDate) {
+        this.startDate = aStartDate;
+        this.endDate = aEndDate;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Date getStartDate()
+    {
+        return startDate;
+    }
+
+    public Date getEndDate()
+    {
+        return endDate;
+    }
+
+}
