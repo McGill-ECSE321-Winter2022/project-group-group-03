fbc66dfccf95e230d207cd2dff1396df4a3ce06d
Abhijeet Praveen <abhijeetpraveen@gmail.com>
2022-03-11 15:02:13 -0500

    Added Business Hours and Item Service classes

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/service/BusinessHourService.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/service/BusinessHourService.java
@@ -2,7 +2,6 @@ package ca.mcgill.ecse321.GroceryStore.service;
 
 import ca.mcgill.ecse321.GroceryStore.dao.BusinessHourRepository;
 import ca.mcgill.ecse321.GroceryStore.model.BusinessHour;
-import ca.mcgill.ecse321.GroceryStore.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,15 +10,49 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class BusinessHourService {
+
     @Autowired
     BusinessHourRepository businessHourRepository;
 
+    private static int curID = 10000;
+
+    @Transactional
     public BusinessHour createBusinessHour(Time startTime, Time endTime, BusinessHour.DayOfWeek aDayOfWeek){
         BusinessHour newBusinessHour = new BusinessHour();
         newBusinessHour.setStartTime(startTime);
         newBusinessHour.setEndTime(endTime);
         newBusinessHour.setDay(aDayOfWeek);
+        newBusinessHour.setHoursID(curID++);
         return newBusinessHour;
+    }
+
+    @Transactional
+    public BusinessHour getBusinessHour(int hoursID) {
+        return businessHourRepository.findByHoursID(hoursID);
+    }
+
+    @Transactional
+    public List<BusinessHour> getAllBusinessHours() {
+        return toList(businessHourRepository.findAll());
+    }
+
+    @Transactional
+    public void deleteBusinessHour(int hoursID) {
+        BusinessHour businessHour = businessHourRepository.findByHoursID(hoursID);
+        if (businessHour == null) {
+            throw new IllegalArgumentException("The business hour with ID: " + hoursID + "does not exist.");
+        } else {
+            businessHourRepository.deleteById(hoursID);
+        }
+    }
+
+    private <T> List<T> toList(Iterable<T> iterable){
+        List<T> resultList = new ArrayList<T>();
+        for (T t : iterable) {
+            resultList.add(t);
+        }
+        return resultList;
     }
 }
--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/service/ItemService.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/service/ItemService.java
@@ -1,5 +1,4 @@
 package ca.mcgill.ecse321.GroceryStore.service;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,5 +6,53 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import ca.mcgill.ecse321.GroceryStore.dao.ItemRepository;
+import ca.mcgill.ecse321.GroceryStore.model.Item;
+
+@Service
 public class ItemService {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Transactional
+    public Item createItem(String name, boolean purchasable, int price, String description, int stock) {
+        Item item = new Item();
+        item.setName(name);
+        item.setPurchasable(purchasable);
+        item.setPrice(price);
+        item.setDescription(description);
+        item.setStock(stock);
+        item.setTotalPurchased(0);
+        itemRepository.save(item);
+        return item;
+    }
+
+    @Transactional
+    public Item getItem(String name) {
+        return itemRepository.findByName(name);
+    }
+
+    @Transactional
+    public List<Item> getAllItems() {
+        return toList(itemRepository.findAll());
+    }
+
+    @Transactional
+    public void deleteItem(String name) {
+        Item item = itemRepository.findByName(name);
+        if (item == null) {
+            throw new IllegalArgumentException("The item with name " + name + "does not exist.");
+        } else {
+            itemRepository.deleteById(name);
+        }
+    }
+
+    private <T> List<T> toList(Iterable<T> iterable){
+        List<T> resultList = new ArrayList<T>();
+        for (T t : iterable) {
+            resultList.add(t);
+        }
+        return resultList;
+    }
 }
