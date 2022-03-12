c6badde94067e9bb53a823e4a6edf3da70447dc4
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2022-03-12 16:17:02 -0500

    added a service, dto, controller class for Order

---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/controller/OrderRestController.java
@@ -0,0 +1,35 @@
+package ca.mcgill.ecse321.GroceryStore.controller;
+
+
+import ca.mcgill.ecse321.GroceryStore.dto.OrderDTO;
+import ca.mcgill.ecse321.GroceryStore.model.Order;
+import ca.mcgill.ecse321.GroceryStore.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class OrderRestController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping(value = {"/order", "/order/"})
+    public List<OrderDTO> getHolidays() throws IllegalArgumentException {
+        List<OrderDTO> orderDTOS = new ArrayList<>();
+        for (Order order : orderService.getAllOrders()) orderDTOS.add(convertToDto(order));
+        return orderDTOS;
+    }
+
+    private OrderDTO convertToDto(Order o) {
+        if (o == null) {
+            throw new IllegalArgumentException("There is no such Holiday!");
+        }
+        return new OrderDTO(o.getConfirmationNumber(), o.getTotalCost(), o.getStore(),o.getPurchasedItem());
+    }
+}
--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/dto/OrderDTO.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/dto/OrderDTO.java
@@ -1,5 +1,39 @@
 package ca.mcgill.ecse321.GroceryStore.dto;
 
+import ca.mcgill.ecse321.GroceryStore.model.PurchasedItem;
+import ca.mcgill.ecse321.GroceryStore.model.Store;
+
+import java.util.List;
+
 public class OrderDTO {
 
+    private int confirmationNumber;
+    private int totalCost;
+
+    //Order Associations
+    private Store store;
+    private List<PurchasedItem> purchasedItem;
+
+    public OrderDTO(int confirmationNumber, int totalCost, Store store, List<PurchasedItem> purchasedItem) {
+        this.confirmationNumber = confirmationNumber;
+        this.totalCost = totalCost;
+        this.store = store;
+        this.purchasedItem = purchasedItem;
+    }
+
+    public int getConfirmationNumber() {
+        return confirmationNumber;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public List<PurchasedItem> getPurchasedItem() {
+        return purchasedItem;
+    }
 }
