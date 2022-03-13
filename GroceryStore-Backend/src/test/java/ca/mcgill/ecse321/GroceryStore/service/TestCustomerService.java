96640fbb967df5c138b9c6bf6d5c26e09d24a793
AriA700 <75389449+AriA700@users.noreply.github.com>
2022-03-13 17:21:44 -0400

    skeleton customer

--- GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/GroceryStore/service/TestCustomerService.java
+++ GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/GroceryStore/service/TestCustomerService.java
@@ -1,4 +1,25 @@
 package ca.mcgill.ecse321.GroceryStore.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import ca.mcgill.ecse321.GroceryStore.dao.CustomerRepository;
+import ca.mcgill.ecse321.GroceryStore.dao.DeliveryOrderRepository;
+import ca.mcgill.ecse321.GroceryStore.dao.PickupOrderRepository;
+import ca.mcgill.ecse321.GroceryStore.model.DeliveryOrder;
+import ca.mcgill.ecse321.GroceryStore.model.PickupOrder;
+import ca.mcgill.ecse321.GroceryStore.model.Customer;
+
+@ExtendWith(MockitoExtension.class)
 public class TestCustomerService {
+
+
 }
