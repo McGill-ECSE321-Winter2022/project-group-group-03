ec8ded4d0f2c85c8aff49fbff1bd5f309b223d23
Abhijeet Praveen <abhijeetpraveen@gmail.com>
2022-03-15 14:54:51 -0400

    Finished ItemRestController with all updates.

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/controller/ItemRestController.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStore/controller/ItemRestController.java
@@ -19,19 +19,53 @@ public class ItemRestController {
     private ItemService service;
 
     @PostMapping(value = { "/item", "/item/" })
-    public ItemDTO createBusinessHour(@RequestParam String itemName ,@RequestParam boolean purchasable,
+    public ItemDTO createItem(@RequestParam String itemName ,@RequestParam boolean purchasable,
                                       @RequestParam int price, @RequestParam String description,
                                       @RequestParam int stock) throws IllegalArgumentException {
         return convertToDto(service.createItem(itemName,purchasable,price,description,stock));
     }
 
     @GetMapping(value = {"/item","/item/"})
-    public List<ItemDTO> getBusinessHours() throws IllegalArgumentException {
+    public List<ItemDTO> getItems() throws IllegalArgumentException {
         return service.getAllItems().stream().map(this::convertToDto).collect(Collectors.toList());
 
     }
 
-    //@PutMapping(value = {"/editItem/{hoursID}"})
+    @PutMapping(value = {"/editItemPurchasable/{itemName}"})
+    public ItemDTO updateItemPurchasable(@PathVariable("itemName") String itemName,
+                                         @RequestParam boolean newPurchasable) throws IllegalArgumentException {
+        return convertToDto(service.updateItemPurchasable(itemName,newPurchasable));
+    }
+
+    @PutMapping(value = {"/editItemDescription/{itemName}"})
+    public ItemDTO updateItemDescription(@PathVariable("itemName") String itemName,
+                                         @RequestParam String newDescription) throws IllegalArgumentException {
+        return convertToDto(service.updateItemDescription(itemName,newDescription));
+    }
+
+    @PutMapping(value = {"/editItemTotalPurchased/{itemName}"})
+    public ItemDTO updateItemTotalPurchased(@PathVariable("itemName") String itemName,
+                                            @RequestParam int newTotalPurchased) throws IllegalArgumentException {
+        return convertToDto(service.updateItemTotalPurchased(itemName,newTotalPurchased));
+    }
+
+    @PutMapping(value = {"/editItemStock/{itemName}"})
+    public ItemDTO updateItemStock(@PathVariable("itemName") String itemName,
+                                   @RequestParam int newStock) throws IllegalArgumentException {
+        return convertToDto(service.updateItemStock(itemName,newStock));
+    }
+
+    @PutMapping(value = {"/addItemStock/{itemName}"})
+    public ItemDTO addItemStock(@PathVariable("itemName") String itemName,
+                                   @RequestParam int addedStock) throws IllegalArgumentException {
+        return convertToDto(service.addItemStock(itemName,addedStock));
+    }
+
+    @PutMapping(value = {"/editItemPrice/{itemName}"})
+    public ItemDTO updateItemPrice(@PathVariable("itemName") String itemName,
+                                @RequestParam int newPrice) throws IllegalArgumentException {
+        return convertToDto(service.updateItemPrice(itemName,newPrice));
+    }
 
     @GetMapping(value = {"/item/{itemName}", "/item/{itemName}/"})
     public ItemDTO getItem(@PathVariable("itemName") String itemName) throws IllegalArgumentException {
