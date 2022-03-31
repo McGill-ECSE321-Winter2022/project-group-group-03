79d8bff167c3c15520c05e34e66a82ae550b844c
AriA700 <75389449+AriA700@users.noreply.github.com>
2022-03-31 17:32:31 -0400

    Owner BH #47

--- GroceryStore-Frontend/src/components/OwnerBH.vue
+++ GroceryStore-Frontend/src/components/OwnerBH.vue
@@ -1,51 +1,154 @@
 <template>
   <div>
     <Header />
-
+    <div class="row">
+      <div id="col5" class="col-6">
+        <table id="table">
+          <tr>
+              <td>Mon.</td>
+              <td><b-button id="hourbutton" v-b-modal.modal-1>{{hours[0].startTime}}</b-button></td>
+              <b-modal id="modal-1" title="Update Hours">
+                <div align="center">
+                  <b-dropdown id="dropdown-left" text="Change opening hour" variant="primary" class="m-2">
+                    <b-dropdown-item href="#">01:00</b-dropdown-item>
+                    <b-dropdown-item href="#">02:00</b-dropdown-item>
+                    <b-dropdown-item href="#">03:00</b-dropdown-item>
+                    <b-dropdown-item href="#">04:00</b-dropdown-item>
+                    <b-dropdown-item href="#">05:00</b-dropdown-item>
+                    <b-dropdown-item href="#">06:00</b-dropdown-item>
+                    <b-dropdown-item href="#">07:00</b-dropdown-item>
+                    <b-dropdown-item href="#">08:00</b-dropdown-item>
+                    <b-dropdown-item href="#">09:00</b-dropdown-item>
+                    <b-dropdown-item href="#">10:00</b-dropdown-item>
+                    <b-dropdown-item href="#">11:00</b-dropdown-item>
+                    <b-dropdown-item href="#">12:00</b-dropdown-item>
+                  </b-dropdown>
+                </div>
+              </b-modal>
+              <td id="am">a.m</td>
+              <div id="topm">
+                <td>To</td>
+                <td><b-button id="hourbutton2" v-b-modal.modal-2>{{hours[0].startTime}}</b-button></td>
+                <b-modal id="modal-2" title="BootstrapVue">
+                <p class="my-4">Hello from !</p>
+                </b-modal>
+                <td id="pm">p.m</td>
+              </div>
+          </tr>
+          <tr>
+              <td>Tues.</td>
+              <td><button id="hourbutton"> {{hours[1].startTime}}</button></td>
+              <td id="am">a.m</td>
+              <div id="topm">
+                <td>To</td>
+                <td><button id="hourbutton2">{{hours[1].startTime}} </button></td>
+                <td id="pm">p.m</td>
+              </div>
+          </tr>
+          <tr>
+            <td>Wed.</td>
+            <td><button id="hourbutton">{{hours[2].startTime}}</button></td>
+            <td id="am">a.m</td>
+            <div id="topm">
+              <td>To</td>
+              <td><button id="hourbutton2">{{hours[2].startTime}}</button></td>
+              <td id="pm">p.m</td>
+            </div>
+          </tr>
+          <tr>
+            <td>Thurs.</td>
+            <td><button id="hourbutton">{{hours[3].startTime}}</button></td>
+            <td id="am">a.m</td>
+            <div id="topm">
+              <td>To</td>
+              <td><button id="hourbutton2">{{hours[3].startTime}}</button></td>
+              <td id="pm">p.m</td>
+            </div>
+          </tr>
+          <tr>
+            <td>Fri.</td>
+            <td><button id="hourbutton">{{hours[4].startTime}} </button></td>
+            <td id="am">a.m</td>
+            <div id="topm">
+              <td>To</td>
+              <td><button id="hourbutton2">{{hours[4].startTime}}</button></td>
+              <td id="pm">p.m</td>
+            </div>
+          </tr>
+          <tr>
+            <td>Sat.</td>
+            <td><button id="hourbutton">{{hours[5].startTime}}</button></td>
+            <td id="am">a.m</td>
+            <div id="topm">
+              <td>To</td>
+              <td><button id="hourbutton2">{{hours[5].startTime}}</button></td>
+              <td id="pm">p.m</td>
+            </div>
+          </tr>
+          <tr>
+            <td>Sun.</td>
+            <td><button id="hourbutton">{{hours[6].startTime}}</button></td>
+            <td id="am">a.m</td>
+            <div id="topm">
+              <td>To</td>
+              <td><button id="hourbutton2">{{hours[6].startTime}}</button></td>
+              <td id="pm">p.m</td>
+            </div>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
+
+
 </template>
 
 
-<script>
+<script src="./businesshours.js">
 import Header from "../components/Header.vue"
 export default {
-  data() {
-    return {
-      form: {
-        email: '',
-        name: '',
-        food: null,
-        checked: []
-      },
-      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-      show: true
-    }
-  },
   components: {
     Header
-  },
-  methods: {
-    onSubmit(event) {
-      event.preventDefault()
-      alert(JSON.stringify(this.form))
-    },
-    onReset(event) {
-      event.preventDefault()
-      // Reset our form values
-      this.form.email = ''
-      this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
-      // Trick to reset/clear native browser form validation state
-      this.show = false
-      this.$nextTick(() => {
-        this.show = true
-      })
-    }
   }
 }
 </script>
 
 <style>
+  #col5{
+    color: red;
+    margin-left: 330px;
+    font-size: xxx-large;
+    margin-top: 60px;
+  }
+  #hourbutton{
+    transition-duration: 0.4s;
+    font-size: xx-large;
+    margin-left: 300px;
+    margin-right: 5px;
+  }
+  #hourbutton:hover{
+    color:white;
+    background-color: red;
+  }
+  #hourbutton2{
+    margin-left: 20px;
+    margin-right: 5px;
+    font-size: xx-large;
+    transition-duration: 0.4s;
+  }
+  #hourbutton2:hover{
+    color:white;
+    background-color: red;
+  }
+  #am{
+    font-size: xx-large;
+    margin-right: 100px;
+  }
+  #pm{
+    font-size: xx-large;
+  }
+  #topm{
+    margin-left: 15px;
+  }
 </style>
 
