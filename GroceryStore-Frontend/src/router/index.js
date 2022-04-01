aea75bcb2797ca0af65a2d0a3b6773972779fd6c
rooshnie23 <90059929+rooshnie23@users.noreply.github.com>
2022-04-01 18:52:48 -0400

    update index.js #41

--- GroceryStore-Frontend/src/router/index.js
+++ GroceryStore-Frontend/src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from '@/components/Login'
-import Employee from '@/components/Employee'
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "../components/Login";
+import Schedule from "../components/Schedule";
+import UpdateEmployee from "../components/UpdateEmployee";
+import UpdateCustomer from "../components/UpdateCustomer";
+import UpdateOwner from "../components/UpdateOwner";
+import Login from '@/components/Login';
+import Employee from '@/components/Employee';
 import Items from "@/components/Items";
 
 
 Vue.use(Router)
 
+
 export default new Router({
   routes: [
-    {
+     {
       path: '/',
       name: 'Login',
       component: Login,
@@ -19,11 +25,33 @@ export default new Router({
       name: 'Employee',
       component: Employee
     },
+   {
+	      path: '/Items',
+	      name: 'Items',
+	      component: Items
+	    },
+
     {
-      path: '/Items',
-      name: 'Items',
-      component: Items
+      path: '/Schedule',
+      name: 'Schedule',
+      component: Schedule,
+    },
+    {
+      path: '/UpdateEmployee',
+      name: 'UpdateEmployee',
+      component: UpdateEmployee,
+    },
+    {
+      path: '/UpdateCustomer',
+      name: 'UpdateCustomer',
+      component: UpdateCustomer,
+    },
+    {
+      path: '/UpdateOwner',
+      name: 'UpdateOwner',
+      component: UpdateOwner,
     }
 
   ]
 })
+
