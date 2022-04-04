ae118726a89ec44fe37e4fdacacb15a6a99732e9
Abhijeet Praveen <abhijeetpraveen@gmail.com>
2022-04-04 06:18:20 -0400

    better error handling in sign up page and other minor stuff #41

--- GroceryStore-Frontend/src/components/Logout.vue
+++ GroceryStore-Frontend/src/components/Logout.vue
@@ -1,15 +1,44 @@
 <template>
-  <body>
-  <main>
-
-    <div style="margin-left: 500px; margin-top: 300px">
-      <h2>You have successfully logged out!</h2>
-        We are sad to see you go 😢 To log back in, please click
-        <router-link style="color: #e03444" :to="{ name: 'Login' }">here</router-link>
+  <div>
+    <br>
+    <br>
+    <br>
+    <div class="logout">
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <h1>You have successfully logged out!</h1>
+      <h4>We are sad to see you go 😢 To login again, please click <router-link id="bold" :to="{ name: 'Login' }">here</router-link></h4>
     </div>
-
-  </main>
-  </body>
+  </div>
 </template>
 
 
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: lighter;
+  text-align: center;
+}
+
+#bold {
+  font-weight: 900;
+  color: #e03444;
+
+}
+
+.logout {
+  position:absolute;
+  width:840px;
+  height:700px;
+  left:50%;
+  top:50%;
+  margin-left:-420px;
+  margin-top:-250px;
+}
+</style>
