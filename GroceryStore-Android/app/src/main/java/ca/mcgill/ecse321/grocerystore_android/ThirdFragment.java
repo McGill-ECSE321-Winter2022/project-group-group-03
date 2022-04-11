3d3d4c1d7a4cafe1763197794264f476a8731e77
seb8stien1 <sebastien.cantin@mail.mcgill.ca>
2022-04-11 15:44:49 -0400

    more work on items page done #69

--- GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore_android/ThirdFragment.java
+++ GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore_android/ThirdFragment.java
@@ -8,15 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import ca.mcgill.ecse321.grocerystore_android.databinding.FragmentSecondBinding;
 import ca.mcgill.ecse321.grocerystore_android.databinding.FragmentThirdBinding;
+import cz.msebera.android.httpclient.Header;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ThirdFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ThirdFragment extends Fragment {
 
     private FragmentThirdBinding binding;
@@ -35,6 +39,32 @@ public class ThirdFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        LinearLayout itemLayout = (LinearLayout) getActivity().findViewById(R.id.ItemsLayout);
+        HttpUtils.get("item/", new RequestParams(), new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response){
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonobject = response.getJSONObject(i);
+                        String name = jsonobject.getString("name");
+                        boolean purchasable = jsonobject.getBoolean("purchasable");
+                        int price = jsonobject.getInt("price");
+                        String description = jsonobject.getString("description");
+                        int stock = jsonobject.getInt("stock");
+                        String url = jsonobject.getString("image");
+                        System.out.println(name+"\n"+purchasable+"\n"+price+"\n"+description+"\n"+stock+"\n"+url);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers,String errorResponse, Throwable throwable){
+                System.out.println(errorResponse);
+            }
+        });
     }
 
     @Override
